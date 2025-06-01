4cb4d48f1b96cd5027e4331e619338695b29388f
Kate Mendoza <16334@coderacademy.edu.au>
2025-06-01 22:53:24 +1000

    try/except in reception

--- reception.py
+++ reception.py
@@ -3,30 +3,48 @@ import string
 from entities import Patient
 
 
- # Add new patient
+# Add new patient
 def add_patient(patient_list):
-    first_name = str(input('Enter first name: ')).strip().capitalize()
-    last_name = str(input('Enter last name: ')).strip().capitalize()
-    age = int(input('Enter age: '))
-    sex = str(input('Enter sex (M/F): ')).strip().capitalize()
-    
-    full_name = (f'{first_name} {last_name}')
-    
+    correct_input = False
+    while correct_input != True:
+        while True:
+            first_name = input('Enter first name: ').strip().capitalize()
+            if first_name: 
+                break
+        while True:
+            last_name = input('Enter last name: ').strip().capitalize()
+            if last_name: 
+                break
+        full_name = (f'{first_name} {last_name}')
+        
+        while True:
+            try:
+                age = int(input("Enter age: ").strip())
+                if age >= 0:
+                    break
+            except ValueError:
+                print("Please enter a valid number for age.")
+
+        while True:
+            try:
+                sex = input("Enter sex (M/F): ").strip().upper()
+                if sex == 'M' or sex == 'F':
+                    break
+            except ValueError:
+                print("Sex must be 'M' or 'F'.")
+        correct_input = True
+                
     if full_name in patient_list:
-        print(f'{full_name} is already an existing patient.')
+                print(f'{full_name} is already an existing patient.')
     else:
         # Generate random ID
         patient_id = (lambda: ''.join(random.choices(string.digits, k=6)))()
         new_patient = Patient(patient_id,full_name,age,sex)
         patient_list.append(new_patient)
-        print(f'{full_name} has been added to Patients\' records.')
+        print(f'{full_name} has been added to Patient records.')
         print(f'{new_patient.getDetails()}')
 
-    # Display all patient records
-    # print('\nAll patients:')
-    # for patient in patient_list:
-    #     print(f'{patient.getDetails()}')
-        
+            
 # to call function to main menu  
 def reception_main_menu(patient_list = []):
     print(f'\n--Welcome to Reception: Main Files--\n')
@@ -53,8 +71,7 @@ def reception_main_menu(patient_list = []):
             #should include error-handling here
     
             
-    # Uncomment to test in isolation
-    # reception_main_menu() 
+   
             
   
     
